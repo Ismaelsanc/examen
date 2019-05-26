bbf8bf0f499e3cab6434c9d093ca13db807f8abd
Ismael <Ismael@192.168.1.41>
2019-05-26 09:36:23 +0200

    añadimos otro println

--- prExamen/src/prExamen/main.java
+++ prExamen/src/prExamen/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world from Github!");
+		
+		System.out.println("Fin de main");
 	}
 
 }
